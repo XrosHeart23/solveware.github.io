381df6ae9c41f781fdb093e9f73be0f32dfaf54e
p0ts0 <abdul_qamil@hotmail.sg>
2022-05-16 15:07:04 +0800

    Added manage menu item page

--- js/boundary/menuUI.js
+++ js/boundary/menuUI.js
@@ -1,4 +1,4 @@
-import { GetMenuItemCtrl, GetMenuCatCtrl } from "../controller/menuController.js";
+import { GetMenuItemCtrl, GetMenuCatCtrl, CreateMenuItemCtrl, SearchMenuItemCtrl, UpdateMenuItemCtrl, SuspendMenuItemCtrl } from "../controller/menuController.js";
 
 export class MenuUI {
     MAX_ORDER_QUANTITY = 10;
@@ -33,7 +33,7 @@ export class MenuUI {
             let columnCount = 0;
             let tr;
             menuItems.forEach((items) => {
-                if (cat.catID === items.itemCategory) {
+                if (cat.catID === items.itemCategory && items.itemStatus == true) {
                     if (columnCount == 0) {
                         tr = menuTable.insertRow();
                     }     
@@ -117,4 +117,251 @@ export class MenuUI {
         document.getElementById(id).value = quantity;
     }
     // =======================================================
+
+    // =============== MANAGE MENU ITEMS =====================
+
+    async createMenuItem(form) {
+        const menuCtrl = new CreateMenuItemCtrl();
+
+        let result = await menuCtrl.doCreateMenuItem(form.menuitemcategory.value, form.menuitemname.value,
+            form.menuitemimagename.value, parseFloat(form.menuitemprice.value));
+
+        let sysMsg;
+        if (result) {
+            form.reset();
+            sysMsg = "Menu Item created";
+        } else {
+            sysMsg = "Menu Item name is taken";
+        }
+
+        document.getElementById("createMenuItem_out").innerHTML = sysMsg;
+    }
+
+    async updateMenuItem(form, menuItemID) {
+        const menuCtrl = new UpdateMenuItemCtrl();
+
+        return await menuCtrl.doUpdateMenuItem(form.itemCategory.value, form.imageName.value,
+            form.itemName.value, parseFloat(form.itemPrice.value), menuItemID);
+    }
+
+    async suspendMenuItem(form, menuItemID) {
+        const menuCtrl = new SuspendMenuItemCtrl();
+        let acctStatus = (form.itemStatus.value.toLowerCase() === "activated") ? true : false;
+        let result = await menuCtrl.doSuspendMenuItem(menuItemID, acctStatus);
+
+        if (result) {
+            return "Menu Item activated";
+        } else {
+            return "Menu Item suspended";
+        }
+    }    
+
+    async searchMenuItem(form, searchData, searchType) {
+        let name = form.itemName.value.toLowerCase();
+
+        const searchCtrl = new SearchMenuItemCtrl();
+        let searchResult = await searchCtrl.doSearchMenuItem(name, searchData);
+
+        if (searchType === "search") {
+            const viewTbl = document.getElementById("view_menuItem");
+            viewTbl.innerHTML = "";
+
+            const tbl = document.getElementById("searchMenuItem_result");
+            tbl.innerHTML = ""; // Clear table
+
+            // Add table header
+            const trhead = tbl.insertRow();
+            let th = document.createElement("th");
+            th.setAttribute("id", "searchMenuItemResult_header");
+            th.setAttribute("class", "searchMenuItemResult_header");
+            th.innerHTML = "Search Result";
+            trhead.appendChild(th);
+
+            // Add table row for each result found
+            searchResult.forEach((row) => {
+                const tr = tbl.insertRow();
+                const tdUser = tr.insertCell();
+                tdUser.appendChild(document.createTextNode(row.itemName))
+                tdUser.setAttribute("class", "viewMenuItem_row");
+
+                tdUser.addEventListener("click", () => {
+                    document.getElementById("searchMenuItem_result").style.display = "none";
+                    document.getElementById("createMenuItemtable").style.display = "none";
+                    document.getElementById("view_menuItem").style.display = "table";
+
+                    sessionStorage.setItem("currentViewMenuItem", JSON.stringify(row));
+                    this.displayMenuItem(row);
+                });
+                
+            });
+        }
+        else {
+            return searchResult;
+        }
+    }
+
+    // Display selected menu item information
+    displayMenuItem(data) {
+        const tbl = document.getElementById("view_menuItem");
+        tbl.innerHTML = ""; // Clear table
+
+        const trhead = tbl.insertRow();
+        let th = document.createElement("th");
+        th.setAttribute("id", "viewMenuItem_header");
+        th.setAttribute("class", "viewMenuItem_header");
+        th.setAttribute("colspan", 2);
+        th.innerHTML = "Update Menu Item";
+        trhead.appendChild(th);
+        
+        // Menu Item Category
+        let tr = tbl.insertRow();
+        let tdKey = tr.insertCell();
+        tdKey.appendChild(document.createTextNode("Category"));
+
+        let tdValue = tr.insertCell();
+        let selectField = document.createElement("select");
+        selectField.setAttribute("name", "itemCategory");
+        selectField.setAttribute("id", "viewMenuItem_category");
+        this.displayCategoryDropDown(selectField, data.itemCategory);
+        tdValue.appendChild(selectField);
+        //================================================
+        
+        // Menu Item Name
+        tr = tbl.insertRow();
+        tdKey = tr.insertCell();
+        tdKey.appendChild(document.createTextNode("Name"));
+
+        tdValue = tr.insertCell();
+        let inputField = document.createElement("input");
+
+        inputField.setAttribute("type", "text");
+        inputField.setAttribute("name", "itemName");
+        inputField.setAttribute("id", "viewMenuItem_name");    
+        inputField.setAttribute("value", data.itemName);
+        tdValue.appendChild(inputField);
+
+        // Error message row
+        tr = tbl.insertRow();
+        tr.setAttribute("class", "errorRow");
+        tr.insertCell();
+        
+        tdValue = tr.insertCell();
+        let errorField = document.createElement("span");
+        errorField.setAttribute("id", "updateMenuItemNameError");
+        tdValue.appendChild(errorField);
+        //================================================
+
+        // Menu Item Image Name
+        tr = tbl.insertRow();
+        tdKey = tr.insertCell();
+        tdKey.appendChild(document.createTextNode("Image Name"));
+
+        tdValue = tr.insertCell();
+        inputField = document.createElement("input");
+        inputField.setAttribute("type", "text");
+        inputField.setAttribute("name", "imageName");
+        inputField.setAttribute("id", "viewMenuItem_imageName");    
+        inputField.setAttribute("value", data.itemImage);
+        tdValue.appendChild(inputField);
+
+        // Error message row
+        tr = tbl.insertRow();
+        tr.setAttribute("class", "errorRow");
+        tr.insertCell();
+        
+        tdValue = tr.insertCell();
+        errorField = document.createElement("span");
+        errorField.setAttribute("id", "updateMenuItemImageNameError");
+        tdValue.appendChild(errorField);
+        //================================================
+
+        // Price
+        tr = tbl.insertRow();
+        tdKey = tr.insertCell();
+        tdKey.appendChild(document.createTextNode("Price"));
+
+        tdValue = tr.insertCell();
+        inputField = document.createElement("input");
+        inputField.setAttribute("type", "text");
+        inputField.setAttribute("name", "itemPrice");
+        inputField.setAttribute("id", "viewMenuItem_price");    
+        inputField.setAttribute("value", data.itemPrice);
+        tdValue.appendChild(inputField);
+
+        // Error message row
+        tr = tbl.insertRow();
+        tr.setAttribute("class", "errorRow");
+        tr.insertCell();
+        
+        tdValue = tr.insertCell();
+        errorField = document.createElement("span");
+        errorField.setAttribute("id", "updateMenuItemPriceError");
+        tdValue.appendChild(errorField);
+        //================================================
+
+        // Status
+        tr = tbl.insertRow();
+        tdKey = tr.insertCell();
+        tdKey.appendChild(document.createTextNode("Status"));
+
+        tdValue = tr.insertCell();
+        inputField = document.createElement("input");
+        inputField.setAttribute("type", "text");
+        inputField.setAttribute("name", "itemStatus");
+        inputField.setAttribute("id", "viewMenuItem_status");
+        inputField.setAttribute("style", "border:0;outline:none;");
+        let status = (data.itemStatus) ? "Activated" : "Suspended";
+        inputField.setAttribute("value", status);
+        inputField.readOnly = true;
+        tdValue.appendChild(inputField);
+        //================================================
+
+        // Update button - Left side
+        tr = tbl.insertRow();
+        let tdLeft = tr.insertCell();
+
+        let btn = document.createElement("button");
+        btn.setAttribute("type", "submit");
+        btn.setAttribute("name", "update");
+        btn.setAttribute("id", "update_menuItem");
+        btn.innerHTML = "Update";
+        tdLeft.appendChild(btn);
+        //===============================================
+
+        // Suspend button - Right side
+        let tdRight = tr.insertCell();
+
+        btn = document.createElement("button");
+        btn.setAttribute("type", "submit");
+        btn.setAttribute("name", "suspend");
+        btn.setAttribute("id", "suspend");
+        status = (data.itemStatus) ? "Suspend" : "Activate";
+        btn.innerHTML = status;
+        tdRight.appendChild(btn);
+        //===============================================
+
+        // Output for form
+        tr = tbl.insertRow();
+        let tdOutput = tr.insertCell();
+        tdOutput.setAttribute("colspan", 2);
+        tdOutput.setAttribute("id", "updateMenuItemOut");
+        tdOutput.setAttribute("class", "updateMenuItemOut");
+    }
+
+    // Display profile in dropdown selection
+    async displayCategoryDropDown(dropdownID, menuItemCategory = "") {
+        let category = await this.getMenuCat();
+        dropdownID.innerHTML = "";
+
+        category.forEach((row) => {
+            const option = document.createElement("option");
+            option.setAttribute("value", row.catID);
+            option.innerHTML = row.catName.charAt(0).toUpperCase() + row.catName.slice(1);
+
+            if (row.catID === menuItemCategory)
+                option.setAttribute("selected", "selected");
+
+            dropdownID.appendChild(option);
+        });    
+    }
 }
